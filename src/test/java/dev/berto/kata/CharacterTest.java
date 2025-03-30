8e21dcb5d0a3993e2bce2f499184638ccbe67328
berto9675 <albrtvd@gmail.com>
2025-03-30 16:49:30 +0200

    I create the test to prove that the character is created with the expected characteristics

--- src/test/java/dev/berto/kata/CharacterTest.java
+++ src/test/java/dev/berto/kata/CharacterTest.java
@@ -1,14 +1,20 @@
 package dev.berto.kata;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CharacterTest {
     
-    @Test
-    public void ExampleTest() {
-        assertEquals(2, 2);
+     @Test
+    @DisplayName("Test that the character is created wit the expected characteristics")
+    public void testCharacterCreatedAsExpected() {
+        Character character = new Character();
+        assertEquals(1000, character.getHealth());
+        assertEquals(1, character.getLevel());
+        assertTrue(character.isAlive());
     }
 
 }
